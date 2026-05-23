5620ca68d78b10f57966111a312dfc36ed3870d2
saikrishna1235 <saikrishnakoyyada20669@gmail.com>
2026-05-23 15:33:09 +0530

    Fixed production API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://crm-backend-2rkm.onrender.com",
+  baseURL: "https://crm-backend-2rkm.onrender.com/api"
 });
 
 export default API;
